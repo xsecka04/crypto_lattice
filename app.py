a02bca10f6e2f3d2b3dc93dc1145d6f27895d0d6
Martin Sečkar <m.seckar@hotmail.com>
2021-10-24 19:23:28 +0200

    Added Unimod button

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
-from bokeh.models import ColumnDataSource, Div, Select, Slider, TextInput, CustomJS, Range1d, Arrow, OpenHead
+from bokeh.models import ColumnDataSource, Div, Select, Slider, TextInput, CustomJS, Range1d, Arrow, OpenHead, TapTool, Button
 from bokeh.io import curdoc
 from bokeh.resources import INLINE
 from bokeh.embed import components
 from bokeh.layouts import row, column
 import numpy as np
 from bokeh.plotting import figure, output_file, show
-
+from bokeh.events import Tap
+import random
 
 # app = Flask(__name__)
 
@@ -30,12 +31,41 @@ def generate_lattice(basis):
 
 def hadamard_ratio(basis):
     dimension = basis.ndim
-    det = np.linalg.det(basis)
+    det = abs(np.linalg.det(basis))
     mult = 1
     for v in basis:
         mult = mult * np.linalg.norm(v)
     hratio = (det / mult) ** (1.0 / dimension)
     return hratio
+
+
+#def rand_unimod(seed,n):
+def rand_unimod(n):
+	#np.random.seed(seed)
+	#random.seed(seed)
+	random_matrix = [ [np.random.randint(-10,10) for _ in range(n) ] for _ in range(n) ]
+	upperTri = np.triu(random_matrix,0)
+	lowerTri = [[np.random.randint(-10,10) if x<y else 0 for x in range(n)] for y in range(n)]  
+
+	for r in range(len(upperTri)):
+		for c in range(len(upperTri)):
+			if(r==c):
+				if bool(random.getrandbits(1)):
+					upperTri[r][c]=1
+					lowerTri[r][c]=1
+				else:
+					upperTri[r][c]=-1
+					lowerTri[r][c]=-1
+	uniModular = np.matmul(upperTri,lowerTri)
+	return uniModular
+
+
+def regenerate_lattice(basis):
+    x, y = generate_lattice(basis)
+    source.data = dict(x=x, y=y)
+    print(f"Had: {hadamard_ratio(basis)}, basis {basis}")
+    bsource.data = dict(x=basis[0], y=basis[1], hadamard=[hadamard_ratio(basis), 0])
+    hadamard.text = f"""Hadamard Ratio: {bsource.data['hadamard'][0]}"""
 
 
 output_file("app.html")
@@ -67,6 +97,14 @@ show(p)
 p.add_layout(Arrow(end=OpenHead(line_color="firebrick", line_width=4),
                    x_start=0, y_start=0, x_end='x', y_end='y', source=bsource))
 
+#TODO: click tool integration
+taptool = p.add_tools(TapTool())
+
+def callback(event):
+    selected = source.selected.indices
+    print(selected)
+
+p.on_event(Tap, callback)
 
 
 def x1_callback(attr, old, new):
@@ -78,10 +116,7 @@ def x1_callback(attr, old, new):
     basis = bsource.data
     basis['x'][0] = new
     newbasis = np.array([basis['x'], basis['y']])
-    x, y = generate_lattice(newbasis)
-    source.data = dict(x=x, y=y)
-    bsource.data = dict(x=newbasis[0], y=newbasis[1], hadamard=[hadamard_ratio(newbasis), 0])
-    hadamard.text = f"""Hadamard Ratio: {bsource.data['hadamard'][0]}"""
+    regenerate_lattice(newbasis)
 
 
 def x2_callback(attr, old, new):
@@ -93,10 +128,7 @@ def x2_callback(attr, old, new):
     basis = bsource.data
     basis['x'][1] = new
     newbasis = np.array([basis['x'], basis['y']])
-    x, y = generate_lattice(newbasis)
-    source.data = dict(x=x, y=y)
-    bsource.data = dict(x=newbasis[0], y=newbasis[1], hadamard=[hadamard_ratio(newbasis), 0])
-    hadamard.text = f"""Hadamard Ratio: {bsource.data['hadamard'][0]}"""
+    regenerate_lattice(newbasis)
 
 def y1_callback(attr, old, new):
     try:
@@ -107,10 +139,7 @@ def y1_callback(attr, old, new):
     basis = bsource.data
     basis['y'][0] = new
     newbasis = np.array([basis['x'], basis['y']])
-    x, y = generate_lattice(newbasis)
-    source.data = dict(x=x, y=y)
-    bsource.data = dict(x=newbasis[0], y=newbasis[1], hadamard=[hadamard_ratio(newbasis), 0])
-    hadamard.text = f"""Hadamard Ratio: {bsource.data['hadamard'][0]}"""
+    regenerate_lattice(newbasis)
 
 
 def y2_callback(attr, old, new):
@@ -122,10 +151,26 @@ def y2_callback(attr, old, new):
     basis = bsource.data
     basis['y'][1] = new
     newbasis = np.array([basis['x'], basis['y']])
-    x, y = generate_lattice(newbasis)
-    source.data = dict(x=x, y=y)
-    bsource.data = dict(x=newbasis[0], y=newbasis[1], hadamard=[hadamard_ratio(newbasis), 0])
-    hadamard.text = f"""Hadamard Ratio: {bsource.data['hadamard'][0]}"""
+    regenerate_lattice(newbasis)
+
+def unimod_callback(event):
+    basis = bsource.data
+    print(basis)
+    try:
+        newbasis = np.array([[basis['x'][2], basis['x'][3]], [basis['y'][2], basis['y'][3]]])
+        print(f"true{newbasis}")
+    except IndexError:
+        newbasis = np.array([basis['x'], basis['y']])
+        print(f"false{newbasis}")
+
+    newbasis = np.matmul(newbasis,rand_unimod(2))
+    print(newbasis)
+    new_data = {
+    'x' : newbasis[0],
+    'y' : newbasis[1],
+    'hadamard': [hadamard_ratio(newbasis), 0]
+    }
+    bsource.stream(new_data)
 
 x1 = Slider(title="X1", value=2, start=-10, end=10, step=1)
 x2 = Slider(title="X2", value=1, start=-10, end=10, step=1)
@@ -147,9 +192,10 @@ x2.on_change('value', x2_callback)
 y2.on_change('value', y2_callback)
 
 hadamard = Div(text=f"""Hadamard Ratio: {bsource.data['hadamard'][0]}""", width=200, height=100)
-show(hadamard)
+button = Button(label="Apply Unimodular matrix", button_type="default")
+button.on_click(unimod_callback)
 
-curdoc().add_root(row(column(x1, x1_input, y1, y1_input, x2, x2_input, y2, y2_input, hadamard), p, width=400))
+curdoc().add_root(row(column(x1, x1_input, y1, y1_input, x2, x2_input, y2, y2_input, button, hadamard), p, width=400))
 curdoc().title = "Lattice-based Cryptography"
 
 #    return render_template('index.html')
