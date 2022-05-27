29bfb17b248a265951b1da978e887d950ad9d0f7
Martin Sečkar <m.seckar@hotmail.com>
2022-05-27 06:28:25 +0200

    changed deployment IP

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,11 @@ FROM python:3.9.7
 WORKDIR /app
 COPY . . 
 
+ENV IP=159.223.216.239
+
 RUN pip install -r requirements.txt
 
-EXPOSE 50000:50000
+EXPOSE 80:80
 EXPOSE 50007:50007
 ENTRYPOINT ["python"]
 CMD ["run.py"]
--- app/views.py
+++ app/views.py
@@ -4,12 +4,12 @@ from flask import jsonify, render_template
 from . import app
 import os
 
-#ip = os.environ.get('IP')
+ip = os.environ.get('IP')
 
 @app.route('/', methods=['GET'])
 def babai_page():
-    #script = server_document(f'http://{ip}:50007/babai')
-    script = server_document(f'http://localhost:50007/babai')
+    script = server_document(f'http://{ip}:50007/babai')
+    #script = server_document(f'http://localhost:50007/babai')
     return render_template("index.html", script=script, template="Flask")
 
 
@@ -23,15 +23,15 @@ def babai_page():
 
 @app.route('/alg', methods=['GET'])
 def alg_page():
-    #script = server_document(f'http://{ip}:50007/babai')
-    script = server_document(f'http://localhost:50007/alg')
+    script = server_document(f'http://{ip}:50007/alg')
+    #script = server_document(f'http://localhost:50007/alg')
     return render_template("alg.html", script=script, template="Flask")
 
 
 @app.route('/lwe_basis', methods=['GET'])
 def lwe_basis_page():
-    #script = server_document(f'http://{ip}:50007/babai')
-    script = server_document(f'http://localhost:50007/lwe_basis')
+    script = server_document(f'http://{ip}:50007/lwe_basis')
+    #script = server_document(f'http://localhost:50007/lwe_basis')
     return render_template("lwe.html", script=script, template="Flask")
 
 
