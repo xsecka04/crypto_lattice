2df6701c253c8cb8e16f8fe5cdf1ea3b37c44261
Martin Sečkar <m.seckar@hotmail.com>
2022-05-30 22:31:56 +0200

    Working download function with semntic changes

--- app/views.py
+++ app/views.py
@@ -4,22 +4,13 @@ from . import app
 import os
 
 ip = os.environ.get('IP')
-ip = "localhost"
+#ip = "localhost"
 
 @app.route('/', methods=['GET'])
 def babai_page():
     script = server_document(f'http://{ip}:50007/babai')
     #script = server_document(f'http://localhost:50007/babai')
     return render_template("index.html", script=script, template="Flask")
-
-
-
-#@app.route('/lwe', methods=['GET'])
-#def lwe_page():
-#    #script = server_document(f'http://{ip}:50007/babai')
-#    script = server_document(f'http://localhost:50008/lwe')
-#    return render_template("index.html", script=script, template="Flask")
-
 
 @app.route('/alg', methods=['GET'])
 def alg_page():
--- run.py
+++ run.py
@@ -7,31 +7,16 @@ from app.babai import babai_app
 from app.algorithm import alg_app
 from app.lwe_basis import lwe_basis_app
 
-
-#from app.lwe import lwe_app
 ip = os.environ.get('IP')
-ip = "localhost"
+#ip = "localhost"
 
 def app_worker():
     server = Server({'/babai' : babai_app, '/alg' : alg_app, '/lwe_basis' : lwe_basis_app}, io_loop=IOLoop(), allow_websocket_origin=[ip], port=50007)
     server.start()
     server.io_loop.start()
 
-#def alg_worker():
-#    server = Server({'/alg' : alg_app}, io_loop=IOLoop(), allow_websocket_origin=["*"], port=50010)
-#    server.start()
-#    server.io_loop.start()
-
-#def lwe_basis_app_worker():
-#    server = Server({'/lwe_basis' : lwe_basis_app}, io_loop=IOLoop(), allow_websocket_origin=["*"], port=50011)
-#    server.start()
-#    server.io_loop.start()
-
 
 Thread(target=app_worker).start()
-#Thread(target=lwe_worker).start()
-#Thread(target=alg_worker).start()
-#Thread(target=lwe_basis_app_worker).start()
 
 
 
